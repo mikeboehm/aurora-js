79dc7cbeb5d1ad1ae7cd4a90479ab0e131ba1b53
Mike Boehm <mike@boehm.com.au>
2015-11-03 19:17:13 +0000

    Renamed frontend JS file

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ function LifxController(){
 	this.BASE_URL = 'http://localhost:8080';
 	this.ENDPOINT_TURNON = '/lights/all/on';
 	this.ENDPOINT_TURNOFF = '/lights/all/off';
+	this.ENDPOINT_TURNOFF = '/test/sunrise';
 };
 
 LifxController.prototype.turnOn = function() {
@@ -21,6 +22,14 @@ LifxController.prototype.turnOff = function() {
 	})
 }
 
+LifxController.prototype.sunrise = function() {
+	var url_suffix = this.ENDPOINT_SUNRISE;
+	$.ajax({
+		url: this.BASE_URL + url_suffix,
+		type: 'GET'
+	})
+}
+
 $( document ).ready(function() {
 	// LifxController.setup();
 	var lifx = new LifxController();
@@ -29,6 +38,10 @@ $( document ).ready(function() {
 	});
 
 	$('#button-turn-off').click(function(){
+		lifx.turnOff();
+	});
+
+	$('#button-sunrise').click(function(){
 		lifx.turnOff();
 	});
 });
