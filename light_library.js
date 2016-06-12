a5af0148a18aa22960387841cbbe2fec54212b2f
Mike Boehm <mike@boehm.com.au>
2016-06-12 19:14:06 +0100

    Added Light and LightLibrary

--- light_library.js
+++ light_library.js
@@ -1,3 +1,5 @@
+var Light = require('./light.js');
+
 var _ = require('lodash/core');
 
 function LightLibrary() {
@@ -5,26 +7,80 @@ function LightLibrary() {
 	self.lights = {};
 
 	self.newLight = function(id) {
-		return {
-			id: id,
-			online: false,
-			fade: {},
-			power: false
-		};
+		var light = new Light(id);
+		self.lights[id] = light;
+
+		return self.lights[id];
+	}
+
+	self.setLightOnline = function(id) {
+		var light = self.getOrCreate(id);
+		light.setOnline();
+	}
+
+	self.setLightOffline = function(id) {
+		var light = self.getOrCreate(id);
+		light.setOffline();
+	}
+
+	self.lightIsOnline = function(id) {
+		var light = self._getLight(id);
+		return light ? light.isOnline() : false;
+	}
+
+	self.setFade = function(id, fade) {
+		var light = self.getOrCreate(id);
+
+		light.setFade(fade);
+	}
+
+	self.getFade = function(id) {
+		var light = self._getLight(id);
+
+		return light ? light.getFade() : false;
+	}
+
+	self.getLightPower = function (lightId) {
+		var light = self._getLight(lightId);
+
+		return light ? light.getPower() : true;
+	}
+
+	self.setLightPowerOn = function (lightId) {
+		self._setLightPower(lightId, true);
+	}
+
+	self.setLightPowerOff = function (lightId) {
+		self._setLightPower(lightId, false);
+	}
+
+	self._setLightPower = function (lightId, powerState) {
+		var light = self._getLight(lightId);
+		light.setPower(powerState);
+	}
+
+	self.getLightPower = function (lightId) {
+		var light = self._getLight(lightId);
+
+		return light ? light.getPower() : false;
+	}
+
+	self.getOrCreate = function(id) {
+		var light = self._getLight(id);
+		return light ? light : self.newLight(id);
 	}
 
 	self._getLight = function(id) {
 		if(
-			typeof id != 'undefined'
-			&& typeof self.lights[id] == 'undefined'
+			typeof self.lights[id] != 'undefined'
 		) {
-			var light = self.newLight(id);
-			self._setLight(light);
+			return self.lights[id];
 		}
-		return self.lights[id];
+		return false;
 	}
 
 	self._setLight = function(light) {
+		console.log(light);
 		if(typeof self.lights[light.id] != 'undefined') {
 			var existing = self._getLight(light.id);
 			var light = _.merge(existing, light);
--- spec/light-library-spec.js
+++ spec/light-library-spec.js
@@ -9,13 +9,74 @@ describe("LightLibrary object", function () {
     });
 });
 
-describe("getters and setters", function() {
-	var lightLibrary = new LightLibrary();
-	it("should add a new empty light when a light doens't exist", function(){
-		var lightId = '1234';
-		var newLight = lightLibrary.newLight(lightId);
-		var missingLight = lightLibrary._getLight(lightId);
+describe("It looks after Lights", function () {
+    it("should be able to create new lights", function () {
+		var lightLibrary = new LightLibrary();
+        var lightId = '12345';
+        var light = lightLibrary.newLight(lightId);
+        expect(light.id).toBe(lightId);
+    });
 
-		expect(missingLight).toEqual(newLight);
-	})
+    it("can return existing lights", function() {
+        var lightLibrary = new LightLibrary();
+        var lightId = '23456';
+        lightLibrary.newLight(lightId);
+
+        var light = lightLibrary._getLight(lightId);
+        expect(light.id).toBe(lightId);
+    });
+
+    it("returns false if it can't find an existing light", function() {
+        var lightLibrary = new LightLibrary();
+        var lightId = 'zzzz';
+        var light = lightLibrary._getLight(lightId);
+        expect(light).toBe(false);
+    });
+
+    it("will safely create a new light when needed", function() {
+        var lightLibrary = new LightLibrary();
+        var lightId = '34567';
+        var light = lightLibrary._getLight(lightId);
+        expect(light).toBe(false);
+        var light = lightLibrary.getOrCreate(lightId);
+        expect(light.id).toBe(lightId);
+    });
+});
+
+describe("It knows if lights are available", function () {
+    it("can set a light as online", function () {
+		var lightLibrary = new LightLibrary();
+        var lightId = '912873';
+        lightLibrary.newLight(lightId);
+        lightLibrary.setLightOnline(lightId);
+        expect(lightLibrary.lightIsOnline(lightId)).toBe(true);
+
+        lightLibrary.setLightOffline(lightId);
+        expect(lightLibrary.lightIsOnline(lightId)).toBe(false);
+    });
+});
+
+describe("It keeps track of how lights SHOULD be", function () {
+    it("knows about power states", function () {
+		var lightLibrary = new LightLibrary();
+        var lightId = '187263';
+        lightLibrary.newLight(lightId);
+        lightLibrary.setLightPowerOn(lightId);
+        expect(lightLibrary.getLightPower(lightId)).toBe(true);
+
+        lightLibrary.setLightPowerOff(lightId);
+        expect(lightLibrary.getLightPower(lightId)).toBe(false);
+    });
+
+    it("knows about fades", function () {
+        var lightLibrary = new LightLibrary();
+        var lightId = '19238';
+        // Light doesn't exist yet
+        expect(lightLibrary.getFade(lightId)).toBe(false);
+
+        lightLibrary.newLight(lightId);
+        var fade = {trousers: 'shorts'};
+        lightLibrary.setFade(lightId, fade);
+        expect(lightLibrary.getFade(lightId)).toBe(fade);
+    });
 });
